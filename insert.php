91891182eb0b44047afc99af876001fea22df0ff
natchayp <73693791+natchayp@users.noreply.github.com>
2020-11-27 17:31:19 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -12,7 +12,7 @@ $w = $_POST['weight'];
 $h = $_POST['height'];
 $bmi = ($w)/(($h/100)*($h/100));
 
-$sql = "INSERT INTO testlabitf27 (name, weight , height , bmi) VALUES ('$n, $w', '$h', '$bmi')";
+$sql = "INSERT INTO testlabitf (name, weight , height , bmi) VALUES ('$n, $w', '$h', '$bmi')";
 
 if (mysqli_query($conn, $sql)) {
     echo "OK";
