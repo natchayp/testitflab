76890b4acc04d673b04b9290ef60f647db4b49ee
natchayp <73693791+natchayp@users.noreply.github.com>
2020-11-27 15:39:28 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,10 +1,14 @@
 <?php
 
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'ihost.it.kmitl.ac.th', 'it63070221', 'IHVvqf58', 'it63070221_natcha', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
 
 $a = $_POST['A'];
 $b = $_POST['B'];
 $c = $a + $b;
-
-echo $c;
 
 ?>
