e79cc830070d18698ba98e74d46ec47d7c340100
natchayp <73693791+natchayp@users.noreply.github.com>
2020-11-27 17:06:13 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -7,17 +7,17 @@ if (mysqli_connect_errno($conn))
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-$a = $_POST['A'];
-$b = $_POST['B'];
-$c = $a + $b;
+$w = $_POST['weight'];
+$h = $_POST['height'];
+$bmi = $w / ($h ** 2);
 
-$sql = "INSERT INTO testlabitf (A , B , C) VALUES ('$a', '$b', '$c')";
+$sql = "INSERT INTO testlabitf27 (weight , height , bmi) VALUES ('$w', '$h', '$bmi')";
 
 if (mysqli_query($conn, $sql)) {
     echo "OK";
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
-  
+
 
 ?>
